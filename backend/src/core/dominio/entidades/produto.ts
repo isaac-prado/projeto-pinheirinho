0dd6e226ec23ae535f88a2428b4e71549127e456
Isaac <isaacpradoalmeida@gmail.com>
2024-11-26 15:25:48 -0300

    fix: corrigido rota adicionarSaldo -> saldo como string

---
+++ backend/src/core/dominio/entidades/produto.ts
@@ -0,0 +1,23 @@
+import PedidoORM from "../../../infra/orm/entidades/PedidoORM";
+
+export default class Produto {
+    id: number;
+    nome: string;
+    estoque: number;
+    preco: number;
+    pedidos: PedidoORM[];
+
+    constructor(
+        id: number,
+        nome: string,
+        estoque: number,
+        preco: number,
+        pedidos: PedidoORM[]
+    ){
+        this.id = id;
+        this.nome = nome;
+        this.estoque = estoque;
+        this.preco = preco;
+        this.pedidos = pedidos;
+    }
+};
