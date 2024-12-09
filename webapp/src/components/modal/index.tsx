df8fecca2b0deb38ecf9353778d116a6ca792a8a
Leonardo Siqueira Fernandes <leonardo.fernandes@rehagro.edu.br>
2024-12-09 17:37:32 -0300

    Revert "ajuste input moeda"
    This reverts commit 6ec727631788c30d00580c783f5c1d72089d51c4.

--- webapp/src/components/modal/index.tsx
+++ webapp/src/components/modal/index.tsx
@@ -1,15 +1,8 @@
 import React, { useState } from 'react';
-import {
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Button,
-  OutlinedInput,
-  InputAdornment,
-} from '@material-ui/core';
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button, TextField, OutlinedInput, InputAdornment } from '@material-ui/core';
 import CheckIcon from '@material-ui/icons/Check';
 import ClearIcon from '@material-ui/icons/Clear';
+import Customer from '../../domain/customer';
 
 interface ModalProps {
   isOpen: boolean;
@@ -20,34 +13,89 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, onSubmit, variant = 'default' }) => {
-  const [value, setValue] = useState<number>(0); 
-  const handleValueChange = (input: string) => {
-    const numericValue = input.replace(/\D/g, '');
-    const updatedValue = parseInt(numericValue || '0', 10);
-    setValue(updatedValue);
-  };
-
-  const formatCurrency = (value: number): string => {
-    return (value / 100).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
-  };
+  const [value, setValue] = useState<number>(0);
+  const [cpf, setCpf] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [address, setAddress] = useState<string>('');
+  const [phone, setPhone] = useState<string>('');
 
   const handleSubmit = () => {
-    onSubmit(value / 100); 
-    setValue(0); 
-    onClose();
+    if (variant === 'register') {
+      let customer = { cpf: cpf, name: name, 
+        address: address, 
+        phone:phone,
+        credit: 0, 
+        isActive: true } as Customer
+      onSubmit(customer);
+      setCpf('');
+      setName('');
+      setAddress('');
+      setPhone('');
+    } else if (variant === 'remove') {
+      onSubmit({ cpf });
+      setCpf('');
+    } else {
+      onSubmit(value);
+      setValue(0);
+    }
+    onClose(); // Fecha o modal após o envio
   };
 
   return (
     <Dialog open={isOpen} onClose={onClose}>
       <DialogTitle>{title}</DialogTitle>
       <DialogContent>
-        {variant === 'default' && (
+        {variant === 'register' ? (
+          <>
+            <TextField
+              label="CPF"
+              fullWidth
+              name='cpf'
+              margin="normal"
+              value={cpf}
+              onChange={(e) => setCpf(e.target.value)}
+            />
+            <TextField
+              label="Nome"
+              fullWidth
+              name='nome'
+              margin="normal"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <TextField
+              label="Endereço"
+              fullWidth
+              name='endereco'
+              margin="normal"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+            <TextField
+              label="Telefone"
+              fullWidth
+              name='telefone'
+              margin="normal"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </>
+        ) : variant === 'remove' ? (
+          <TextField
+            label="CPF"
+            fullWidth
+            name='cpf'
+            margin="normal"
+            value={cpf}
+            onChange={(e) => setCpf(e.target.value)}
+          />
+        ) : (
           <OutlinedInput
             startAdornment={<InputAdornment position="start">R$</InputAdornment>}
             fullWidth
-            name="valor"
-            value={formatCurrency(value)}
-            onChange={(e) => handleValueChange(e.target.value)}
+            name='valor'
+            value={value}
+            onChange={(e) => setValue(Number(e.target.value))}
           />
         )}
       </DialogContent>
